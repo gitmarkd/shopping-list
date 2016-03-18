cbaeecb3bbd38ca6a385535d1dafe1976a6ced0e
Mark <mark.daniels@email.cerrocoso.edu>
2016-03-18 15:05:02 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/* ======================================
+RE-USABLE FUNCTIONS
+=========================================*/
+
+/* add items to the input box, by clicking on the item */
+function addToInput(item) {
+    //    alert('item added to input: ' + item);
+    $('.add-item').val(item);
+}
+
+/* add items to the list, by clicking on the cart */
+function addToList() {}
+
+/* select items to cross out */
+function checkItem() {}
+
+/* remove item from list by clicking the 'X' button */
+function removeItem() {}
+
+/* reset and clear the list */
+function removeAll() {}
+
+/* ======================================
+INSIDE $(document).ready(): functions that target elements that were created during load time
+=========================================*/
+
+$(document).ready(function () {
+
+    /* clicking the item image activates the addToList() function */
+
+    /* clicking the item image activates the addToList() function */
+});
+
+/* ======================================
+KEYBOARD EVENT HANDLERS
+=========================================*/
+
+/* add item on enter */
+
+/* clear list on delete */
+
+/* clear list on backspace */
+
+/* ======================================
+OUTSIDE $(document).ready(): functions that target dynamically created elements that were created after load time
+=========================================*/
