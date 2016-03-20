a65466b733ed9c1d339f89eb5f4cbd9dedc2eefd
Mark <mark.daniels@email.cerrocoso.edu>
2016-03-20 00:46:24 -0700

    Added strikethrough and delete functionality

--- js/main.js
+++ js/main.js
@@ -12,27 +12,37 @@ function addToList(item) {
     $('.shop-list tbody').append(
         '<tr>' +
         '<td class="text-center set-width">' +
-        '<input type="checkbox" class="checkbox"></td>' +
+        '<input type="checkbox" class="checkbox">' +
+        '</td>' +
         '<td class="text-center item-listed">' +
         item +
         '</td>' +
         '<td class="set-width">' +
-        '<button class = "text-center">X</button>' +
+        '<button class="text-center delete">X</button>' +
         '</td>' +
         '</tr>'
     );
+
+    $('#item-name').val('');
 }
 
 /* select items to cross out */
-function checkItem() {
-    alert('you checked the check-box!');
+function strikeItem() {
+    $(this).parent().parent().toggleClass('strike');
 }
 
 /* remove item from list by clicking the 'X' button */
-function removeItem() {}
+function removeItem() {
+    $(this).parent().parent().remove();
+    if ($('.shop-list tbody').children().length == 0) {
+        $('table thead').hide();
+    }
+}
 
 /* reset and clear the list */
-function removeAll() {}
+function removeAll() {
+    $(this).remove();
+}
 
 /* ======================================
 INSIDE $(document).ready(): functions that target elements that were created during load time
@@ -43,10 +53,18 @@ $(document).ready(function () {
     /* clicking the cart invokes the addToList() function */
     $('form a').on('click', function () {
         addToList($('input').val());
+        $('.table thead').show();
         $('.shop-list').show();
     });
 
 });
+
+/* ======================================
+OUTSIDE $(document).ready(): functions that target dynamically created elements that were created after load time
+=========================================*/
+
+$(document).on('click', '.checkbox', strikeItem);
+$(document).on('click', '.delete', removeItem);
 
 /* ======================================
 KEYBOARD EVENT HANDLERS
@@ -57,9 +75,3 @@ KEYBOARD EVENT HANDLERS
 /* clear list on delete */
 
 /* clear list on backspace */
-
-/* ======================================
-OUTSIDE $(document).ready(): functions that target dynamically created elements that were created after load time
-=========================================*/
-
-$(document).on('check', '.checkbox', checkItem);
